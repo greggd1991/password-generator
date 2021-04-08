392153d9dc460a71d4cd7b1fac780579f37c675c
Greg Davis <gregdavis@Gregs-MacBook-Pro.local>
2021-04-07 22:19:35 -0400

    initial html and beginning js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+//user input vars:
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+//Special Characters
+character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ";", ":", "'", "", "<", ",", ">", ".", "/", "?", "~"];
+//Number Characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+//Alphabetic Characters
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Space for uppercase conversion
+space = []; 
+
+var choices;
+//convert letters to uppercase
